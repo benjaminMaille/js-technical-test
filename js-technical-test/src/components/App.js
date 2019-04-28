cf9d264043769464682ad064442db4b3e2398719
Benjamin Maille <Benjamin Maille>
2019-04-28 16:32:57 +0200

    ajout fn load et transmission au cmpt CommentList via props

--- js-technical-test/src/components/App.js
+++ js-technical-test/src/components/App.js
@@ -3,10 +3,35 @@ import React from 'react';
 import SearchForm from './SearchForm';
 import CommentList from './CommentList';
 
+function load(url, callback) {
+    var xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4) {
+            callback(JSON.parse(xhr.response));
+        }
+    }
+
+    xhr.open('GET', url, true);
+    xhr.send('');
+}
+
 class App extends React.Component {
 
     constructor(props) {
         super(props);
+
+        this.state = {comments: []};
+    }
+
+    componentDidMount() {
+        load(
+            'https://api.github.com/repos/octocat/Hello-World/issues/comments',
+            response => {
+                console.log(response);
+                this.setState({comments: response});
+            }
+        );
     }
 
     onSearch(query) {
@@ -17,7 +42,7 @@ class App extends React.Component {
         return (
             <div>
                 <SearchForm onSubmit={this.onSearch} />
-                <CommentList />
+                <CommentList comments={this.state.comments} />
             </div>
         );
     }
